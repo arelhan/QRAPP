648afca11afd0b6d3fae5fa340dfe495edfd80d3
arelhan <are255gu@gmail.com>
2024-09-27 14:23:34 +0300

    Add project files.

---
+++ QR_APP/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace QR_APP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.btn_Create = new System.Windows.Forms.Button();
+            this.btn_quit = new System.Windows.Forms.Button();
+            this.lbl_head = new System.Windows.Forms.Label();
+            this.lbl_info = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_url
+            // 
+            this.txt_url.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_url.Location = new System.Drawing.Point(12, 37);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(472, 26);
+            this.txt_url.TabIndex = 0;
+            // 
+            // btn_Create
+            // 
+            this.btn_Create.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn_Create.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Create.Location = new System.Drawing.Point(12, 74);
+            this.btn_Create.Name = "btn_Create";
+            this.btn_Create.Size = new System.Drawing.Size(256, 64);
+            this.btn_Create.TabIndex = 1;
+            this.btn_Create.Text = "Oluştur";
+            this.btn_Create.UseVisualStyleBackColor = false;
+            this.btn_Create.Click += new System.EventHandler(this.btn_Create_Click);
+            // 
+            // btn_quit
+            // 
+            this.btn_quit.BackColor = System.Drawing.Color.Red;
+            this.btn_quit.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_quit.Location = new System.Drawing.Point(274, 74);
+            this.btn_quit.Name = "btn_quit";
+            this.btn_quit.Size = new System.Drawing.Size(210, 64);
+            this.btn_quit.TabIndex = 2;
+            this.btn_quit.Text = "X";
+            this.btn_quit.UseVisualStyleBackColor = false;
+            this.btn_quit.Click += new System.EventHandler(this.btn_quit_Click);
+            // 
+            // lbl_head
+            // 
+            this.lbl_head.AutoSize = true;
+            this.lbl_head.Location = new System.Drawing.Point(166, 9);
+            this.lbl_head.Name = "lbl_head";
+            this.lbl_head.Size = new System.Drawing.Size(153, 25);
+            this.lbl_head.TabIndex = 3;
+            this.lbl_head.Text = "QR Oluşturucu";
+            // 
+            // lbl_info
+            // 
+            this.lbl_info.AutoSize = true;
+            this.lbl_info.Location = new System.Drawing.Point(12, 141);
+            this.lbl_info.Name = "lbl_info";
+            this.lbl_info.Size = new System.Drawing.Size(203, 25);
+            this.lbl_info.TabIndex = 4;
+            this.lbl_info.Text = "Lütfen bir URL girin!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(420, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "arelhanV1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 193);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_info);
+            this.Controls.Add(this.lbl_head);
+            this.Controls.Add(this.btn_quit);
+            this.Controls.Add(this.btn_Create);
+            this.Controls.Add(this.txt_url);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.Button btn_Create;
+        private System.Windows.Forms.Button btn_quit;
+        private System.Windows.Forms.Label lbl_head;
+        private System.Windows.Forms.Label lbl_info;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ QR_APP/Form1.cs
@@ -0,0 +1,122 @@
+﻿using QRCoder;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace QR_APP
+{
+    public partial class Form1 : Form
+    {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+        public Form1()
+        {
+            InitializeComponent();
+            // Formun boş alanlarına tıklama olayını ayarlayın
+            this.MouseDown += new MouseEventHandler(Form_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form_MouseMove);
+            this.MouseUp += new MouseEventHandler(Form_MouseUp);
+        }
+
+
+        private void btn_Create_Click(object sender, EventArgs e)
+        {
+            // QR koduna eklenmek istenen link
+            string url = txt_url.Text;
+            
+
+            if (string.IsNullOrEmpty(url))
+            {
+                lbl_info.Text = "Lütfen bir URL girin."; // Hata mesajını Label üzerinde göster
+                return;
+            }
+
+            // QR kodu oluşturucu
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                try
+                {
+                    // QR kod verilerini oluştur
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+
+                    // QR kodunu Bitmap olarak oluştur
+                    using (QRCode qrCode = new QRCode(qrCodeData))
+                    {
+                        using (Bitmap qrCodeImage = qrCode.GetGraphic(20)) // 20, modül başına piksel sayısı
+                        {
+                            // SaveFileDialog ile kaydedilecek yeri ve dosya adını sor
+                            SaveFileDialog saveFileDialog = new SaveFileDialog
+                            {
+                                Filter = "PNG Dosyası|*.png",
+                                Title = "QR Kodunu Kaydet",
+                                FileName = "AREqrcode_"+DateTime.Now.ToString("yyyy_MM_dd_HHmmss")+"_.png",  // Varsayılan dosya adı
+                                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)  // Varsayılan klasör Masaüstü
+                            };
+
+                            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                // Dosyayı PNG olarak kaydet
+                                qrCodeImage.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                                lbl_info.Text = "QR kodu başarıyla kaydedildi: ";
+
+                                // Dosyanın bulunduğu klasörü aç
+                                string folderPath = Path.GetDirectoryName(saveFileDialog.FileName);
+                                if (!string.IsNullOrEmpty(folderPath))
+                                {
+                                    Process.Start("explorer.exe", folderPath);
+                                }
+                            }
+                            else
+                            {
+                                lbl_info.Text = "Dosya kaydedilmedi.";
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    lbl_info.Text = $"QR kod oluşturulamadı";  // Hata mesajını Label'a yazdır
+                }
+            }
+        }
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Eğer sol fare butonuna tıklanmışsa
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = true;
+                // Tıklanan konumu ve formun konumunu kaydet
+                dragCursorPoint = Cursor.Position;
+                dragFormPoint = this.Location;
+            }
+        }
+
+        private void Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Eğer form taşınıyorsa
+            if (dragging)
+            {
+                // Formu yeni konuma taşı
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        private void Form_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Fare tuşu bırakıldığında sürüklemeyi durdur
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = false;
+            }
+        }
+
+        private void btn_quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
